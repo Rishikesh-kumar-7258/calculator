ff4de32b8b49d4a90dc7f2cc3acc3a023dc1eb07
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2022-06-09 21:04:10 +0530

    extracting coefficient of variables, just sign is remaining

--- src/components/equation.jsx
+++ src/components/equation.jsx
@@ -1,12 +1,62 @@
-import { Container, Stack, TextareaAutosize, Button } from "@mui/material";
+import {
+  Container,
+  Stack,
+  TextareaAutosize,
+  Button,
+  TextField,
+} from "@mui/material";
 import React from "react";
 
 const EquationSolver = () => {
   const [equation, setEquation] = React.useState("");
+  const [variables, setVariables] = React.useState("");
   const solveEquation = () => {
-    console.log(equation);
+    // Extracting individual equations and varaibles
     let equationArray = equation.split("\n");
-    console.log(equationArray);
+    let variableArray = variables.split(",");
+    let constantArray = [];
+    let coefficientMatrix = [];
+
+    for (let i = 0; i < equationArray.length; i++) {
+      let equation = equationArray[i];
+      constantArray.push(equation.split("=")[1]);
+      equation = cleanEquation(equation.split("=")[0], variableArray);
+      coefficientMatrix.push(getVariables(equation, variableArray));
+    }
+
+    console.log(coefficientMatrix);
+  };
+
+  const cleanEquation = (equation, variables) => {
+    for (let i = 0; i < variables.length; i++) {
+      // if the current variable is not present in the equation
+      var regex = new RegExp(`${variables[i]}`, "gi");
+      let position = equation.search(regex);
+      if (position === -1) {
+        equation += `+0${variables[i]}`;
+      }
+
+      // if the current variale is present but digit is not present
+      regex = new RegExp(`\\d*(?=${variables[i]})`, "gi");
+      let match = equation.match(regex)[0];
+      if (match === "") {
+        equation = equation.replace(regex, "1");
+      }
+    }
+
+    return equation;
+  };
+
+  const getVariables = (equation, variables) => {
+    let variableArray = [];
+    for (let i = 0; i < variables.length; i++) {
+      let regex = new RegExp(`\\d*(?=${variables[i]})`, "gi");
+      let match = equation.match(regex)[0];
+      variableArray.push(match);
+    }
+    // return variableArray;
+    // console.log(variableArray);
+    return variableArray;
   };
 
   return (
@@ -18,6 +68,13 @@ const EquationSolver = () => {
           value={equation}
           onChange={(e) => setEquation(e.target.value)}
         ></TextareaAutosize>
+        <TextField
+          variant="filled"
+          label="Enter the names of variables. e.g. x,y,z"
+          color="primary"
+          value={variables}
+          onChange={(e) => setVariables(e.target.value)}
+        />
         <Button variant="contained" size="large" onClick={solveEquation}>
           Solve
         </Button>
